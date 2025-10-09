32505cf7abea5a4a90dfdf2f4023067772f2db05
Amit Singh <amitsingh19975@gmail.com>
2025-10-09 23:18:17 +0530

    add span

--- include/big_num/internal/mul/toom_cook.hpp
+++ include/big_num/internal/mul/toom_cook.hpp
@@ -5,6 +5,7 @@
 #include "../base.hpp"
 #include "../div/naive.hpp"
 #include "../logical_bitwise.hpp"
+#include "big_num/internal/integer_parse.hpp"
 #include "naive.hpp"
 #include <algorithm>
 #include <memory_resource>
@@ -43,6 +44,15 @@ namespace big_num::internal {
             auto rm = std::span(rhs + lsz, lsz);
             auto rr = std::span(rhs + mid, rsz);
 
+            // auto helper = [](std::span<uint_t const> t) {
+            //     auto tmp = std::vector<uint_t>(t.begin(), t.end());
+            //     twos_complement(tmp);
+            //     return tmp;
+            // };
+
+            BIG_NUM_TRACE(std::println("ll: {}\nlm: {}\nlr: {}", ll, lm, lr));
+            BIG_NUM_TRACE(std::println("rl: {}\nrm: {}\nrr: {}", rl, rm, rr));
+
             using int_t = std::pmr::vector<uint_t>;
             auto l_n2 = int_t{resource};
             auto l_n1 = int_t{resource};
@@ -66,14 +76,16 @@ namespace big_num::internal {
                 std::span<Integer::value_type const> m0, // lower
                 std::span<Integer::value_type const> m1, // middle
                 std::span<Integer::value_type const> m2, // upper 
-                int_t& p_n2, // p(-2)
-                int_t& p_n1, // p(-1)
-                int_t& p_0,  // p(0)
-                int_t& p_1,  // p(1)
-                int_t& p_inf // p(inf)
-            ) -> void {
+                int_t& p_n2,  // p(-2)
+                int_t& p_n1,  // p(-1)
+                int_t& p_0,   // p(0)
+                int_t& p_1,   // p(1)
+                int_t& p_inf, // p(inf)
+                bool& sn1,
+                bool& sn2
+            ) -> bool {
                 // 1. pt = m0 + m2;
-                auto pt = int_t{m0.size() + m1.size() + 1, 0, resource};
+                auto pt = int_t{std::max(m0.size(), m2.size()) + 1, 0, resource};
                 abs_add(pt, m0, m2);
 
                 // 2. p(0) = m0
@@ -81,31 +93,45 @@ namespace big_num::internal {
                 std::copy(m0.begin(), m0.end(), p_0.begin());
 
                 // 3. p(1) = pt + m1
-                p_1.resize(m0.size() + m1.size() + 1, 0);
+                p_1.resize(std::max(pt.size(), m1.size()) + 1, 0);
                 abs_add(p_1, pt, m1);
 
                 // 4. p(-1) = pt - m1
                 p_n1.resize(std::max(pt.size(), m1.size()), 0);
-                abs_sub(p_n1, pt, m1);
+                sn1 = abs_sub(p_n1, pt, m1);
+                // if (cn1) twos_complement(p_n1);
 
                 // 5. p(-2) = (p(-1) + m2) * 2 - m0
-                p_n2.resize(pt.size() + m1.size() + 1, 0);
-                abs_add(p_n2, p_n1, m2);
+                p_n2.resize(std::max(p_n1.size(), m1.size()) + 1, 0);
+                if (!sn1) abs_add(p_n2, p_n1, m2);
+                else sn2 = abs_sub(p_n2, m2, p_n1);
                 shift_left<1>(p_n2);
-                abs_sub(p_n2, m0);
+                if (sn2) {
+                    abs_add(p_n2, m0);
+                } else {
+                    pt.resize(p_n2.size());
+                    std::copy(p_n2.begin(), p_n2.end(), pt.begin());
+                    sn2 = abs_sub(p_n2, pt, m0);
+                }
 
                 // 2. p(inf) = m2
                 p_inf.resize(m2.size());
                 std::copy(m2.begin(), m2.end(), p_inf.begin());
+                return false;
             };
 
-            eval(ll, lm, lr, l_n2, l_n1, l_0, l_1, l_inf);
-            eval(rl, rm, rr, r_n2, r_n1, r_0, r_1, r_inf);
+            bool sln1 = false;
+            bool sln2 = false;
+            bool srn1 = false;
+            bool srn2 = false;
+            eval(ll, lm, lr, l_n2, l_n1, l_0, l_1, l_inf, sln1, sln2);
+            eval(rl, rm, rr, r_n2, r_n1, r_0, r_1, r_inf, srn1, srn2);
 
-            // std::println("ll: {}\nlm: {}\nlr: {}\nln2: {}\nln1: {}\nl0: {}\nl1: {}\nlinf: {}", ll, lm, lr, l_n2, l_n1, l_0, l_1, l_inf);
-            // std::println("rl: {}\nrm: {}\nrr: {}\nrn2: {}\nrn1: {}\nr0: {}\nr1: {}\nrinf: {}", rl, rm, rr, r_n2, r_n1, r_0, r_1, r_inf);
+            BIG_NUM_TRACE(std::println("\n\nln2: {}{}\nln1: {}{}\nl0: {}\nl1: {}\nl_inf: {}\n", sln2 ? '-' : '+', l_n2, sln2 ? '-' : '+', l_n1, l_0, l_1, l_inf));
+            BIG_NUM_TRACE(std::println("\n\nrn2: {}{}\nrn1: {}{}\nr0: {}\nr1: {}\nr_inf: {}\n", srn2 ? '-' : '+', r_n2, srn2 ? '-' : '+', r_n1, r_0, r_1, r_inf));
 
             // out(-2) = l_n2 * r_n2
+            bool son2 = static_cast<bool>(sln2 ^ srn2);
             toom_cook_3_helper<NaiveThreshold>(
                 o_n2.data(), o_n2.size(),
                 l_n2.data(), r_n2.data(),
@@ -113,6 +139,7 @@ namespace big_num::internal {
             );
 
             // out(-1) = l_n1 * r_n1
+            bool son1 = static_cast<bool>(sln1 ^ srn1);
             toom_cook_3_helper<NaiveThreshold>(
                 o_n1.data(), o_n1.size(),
                 l_n1.data(), r_n1.data(),
@@ -139,7 +166,68 @@ namespace big_num::internal {
                 l_inf.data(), r_inf.data(),
                 lsz
             );
-            // std::println("on2: {}\non1: {}\no0: {}\no1: {}\noinf: {}", o_n2, o_n1, o_0, o_1, o_inf);
+            BIG_NUM_TRACE(std::println("on2: {}\non1: {}\no0: {}\no1: {}\noinf: {}", o_n2, o_n1, o_0, o_1, o_inf));
+
+            auto add1 = [](
+                std::span<uint_t> out,
+                bool so,
+                std::span<uint_t const> rhs,
+                bool sr
+            ) {
+                if ((so ^ sr) != 0) {
+                    so = abs_sub(out, rhs);
+                } else {
+                    abs_add(out, rhs);
+                }
+                return so;
+            };
+
+            auto sub1 = [](
+                std::span<uint_t> out,
+                bool so,
+                std::span<uint_t const> rhs,
+                bool sr
+            ) {
+                // out = out - rhs
+                if (so && sr) {
+                    // out = -(out - rhs)
+                    auto c = abs_sub(out, rhs);
+                    so = c == 0;
+                } else if (!so && sr) {
+                    // out = out + rhs
+                    abs_add(out, rhs);
+                } else if (so && !sr) {
+                    // out = out - rhs
+                    so = abs_sub(out, rhs);
+                } else {
+                    so = abs_sub(out, rhs);
+                }
+                return so;
+            };
+
+            auto sub2 = [](
+                std::span<uint_t> out,
+                std::span<uint_t const> lhs,
+                bool sl,
+                std::span<uint_t const> rhs,
+                bool sr
+            ) {
+                auto so = sl;
+                // out = out - rhs
+                if (sl && sr) {
+                    // out = rhs - lhs
+                    so = abs_sub(out, rhs, lhs);
+                } else if (!sl && sr) {
+                    // out = out + rhs
+                    abs_add(out, lhs, rhs);
+                } else if (sl && !sr) {
+                    // out = out - rhs
+                    so = abs_sub(out, lhs, rhs);
+                } else {
+                    so = abs_sub(out, lhs, rhs);
+                }
+                return so;
+            };
 
             // 1. o0 = o_0;
             auto o0 = std::span(o_0);
@@ -148,40 +236,120 @@ namespace big_num::internal {
 
             // 3. o3 = (o_n2 - o_1) / 3
             auto o3 = std::span(o_n2);
-            abs_sub(o3, o_1);
+            auto so3 = sub1(o3, son2, o_1, false);
             naive_div<3>(o3);
+            BIG_NUM_TRACE(std::println("3: {}{}", so3 ? '-' : '+', o3));
 
             // 4. o1 = (o_1 - o_n1) / 2
             auto o1 = std::span(o_1);
-            abs_sub(o1, o_n1);
+            auto so1 = sub1(o1, false, o_n1, son1);
             naive_div<2>(o1);
+            BIG_NUM_TRACE(std::println("4: {}{}", so1 ? '-' : '+', o1));
 
             // 5. o2 = o_n1 - o_0;
             auto o2 = std::span(o_n1);
-            abs_sub(o2, o_0);
+            auto so2 = sub1(o2, son1, o_0, false);
+            BIG_NUM_TRACE(std::println("5: {}", so2 ? '-' : '+', o2));
 
             // 6. o3 = (o2 - o3)/2 + 2 * o_inf;
-            abs_sub(o3, o2, o3);
+            so3 = sub2(o3, o2, so2, o3, so3);
             naive_div<2>(o3);
-            abs_add(o3, o_inf);
-            abs_add(o3, o_inf);
+            so3 = add1(o3, so3, o_inf, false);
+            so3 = add1(o3, so3, o_inf, false);
+            BIG_NUM_TRACE(std::println("6: {}", so3 ? '-' : '+', o3));
 
             // 7. o2 = o2 + o1 - o4
-            abs_add(o2, o1);
-            abs_sub(o2, o4);
+            add1(o2, so2, o1, so1);
+            so2 = sub1(o2, so2, o4, false);
+            BIG_NUM_TRACE(std::println("7: {}", so2 ? '-' : '+', o2));
 
 
             // 8. o1 = o1 - o3
-            abs_sub(o1, o3);
+            so1 = sub1(o1, so1, o3, so3);
+            BIG_NUM_TRACE(std::println("8: {}", so1 ? '-' : '+', o1));
+
+            BIG_NUM_TRACE(std::println("o0: {}\no1: {}\no2: {}\no3: {}\no4: {}", o0, o1, o2, o3, o4));
 
             // 9. out = o4 * x^4k + o3 * x^3k + o2 * x^2k + o1 * x^k + o0
-            abs_add({ out + 0 * lsz, out_size - 0 * lsz }, o0);
-            abs_add({ out + 1 * lsz, out_size - 1 * lsz }, o1);
-            abs_add({ out + 2 * lsz, out_size - 2 * lsz }, o2);
-            abs_add({ out + 3 * lsz, out_size - 3 * lsz }, o3);
-            abs_add({ out + 4 * lsz, out_size - 4 * lsz }, o4);
+            add1({ out + 0 * lsz, out_size - 0 * lsz }, false, o0, false);
+            add1({ out + 1 * lsz, out_size - 1 * lsz }, false, o1, so1);
+            add1({ out + 2 * lsz, out_size - 2 * lsz }, false, o2, so2);
+            add1({ out + 3 * lsz, out_size - 3 * lsz }, false, o3, so3);
+            add1({ out + 4 * lsz, out_size - 4 * lsz }, false, o4, false);
+            BIG_NUM_TRACE(std::println("O: {}", std::span(out, out_size)));
         }
     } // namespace detail
+
+    template <std::size_t NaiveThreshold = MachineConfig::naive_mul_threshold>
+    inline static constexpr auto toom_cook_3(
+        std::span<Integer::value_type> out,
+        std::span<Integer::value_type const> lhs,
+        std::span<Integer::value_type const> rhs,
+        std::pmr::memory_resource* resource = std::pmr::get_default_resource()
+    ) -> void {
+        using uint_t = MachineConfig::uint_t;
+        auto sz = std::max(lhs.size(), rhs.size());
+        sz += 3 - sz % 3; // round up to 3
+
+        auto res = out;
+
+        auto alloc = std::pmr::polymorphic_allocator<Integer::value_type>{resource};
+        auto tmp = std::pmr::vector<uint_t>{resource};
+        auto a = std::pmr::vector<uint_t>{sz, 0, resource};
+        auto b = std::pmr::vector<uint_t>{sz, 0, resource};
+
+        if (sz * 2 > out.size()) {
+            tmp.resize(sz << 1, 0);
+            res = tmp;
+        }
+
+        std::copy_n(lhs.data(), lhs.size(), a.begin());
+        std::copy_n(rhs.data(), rhs.size(), b.begin());
+
+        detail::toom_cook_3_helper<NaiveThreshold + (3 - NaiveThreshold % 3)>(
+            res.data(), res.size(),
+            a.data(), b.data(),
+            sz
+        );
+
+        if (out.data() != res.data()) {
+            std::copy_n(res.begin(), out.size(), out.begin());
+        }
+    }
+
+    template <std::size_t NaiveThreshold = MachineConfig::naive_mul_threshold>
+    inline static constexpr auto toom_cook_3_square(
+        std::span<Integer::value_type> out,
+        std::span<Integer::value_type const> a,
+        std::pmr::memory_resource* resource = std::pmr::get_default_resource()
+    ) -> void {
+        using uint_t = MachineConfig::uint_t;
+        auto sz = a.size();
+        sz += 3 - sz % 3; // round up to 3
+
+        auto res = out;
+
+        auto alloc = std::pmr::polymorphic_allocator<Integer::value_type>{resource};
+        auto tmp = std::pmr::vector<uint_t>{sz, 0, resource};
+        auto tmp0 = std::pmr::vector<uint_t>{resource};
+
+        std::copy_n(a.data(), a.size(), tmp.begin());
+
+        if (sz * 2 > out.size()) {
+            tmp0.resize(sz << 1, 0);
+            res = tmp0;
+        }
+
+        detail::toom_cook_3_helper<NaiveThreshold + (3 - NaiveThreshold % 3)>(
+            res.data(), res.size(),
+            tmp.data(), tmp.data(),
+            sz
+        );
+
+        if (out.data() != res.data()) {
+            std::copy_n(res.begin(), out.size(), out.begin());
+        }
+    }
 
     template <std::size_t NaiveThreshold = MachineConfig::naive_mul_threshold>
     inline static constexpr auto toom_cook_3(
@@ -208,26 +376,20 @@ namespace big_num::internal {
 
     template <std::size_t NaiveThreshold = MachineConfig::naive_mul_threshold>
     inline static constexpr auto toom_cook_3(
-        std::span<Integer::value_type> out,
-        std::span<Integer::value_type const> lhs,
-        std::span<Integer::value_type const> rhs,
+        Integer& out,
+        Integer const& a,
         std::pmr::memory_resource* resource = std::pmr::get_default_resource()
     ) -> void {
-        using uint_t = MachineConfig::uint_t;
-        auto sz = std::max(lhs.size(), rhs.size());
+        auto sz = a.size();
         sz += 3 - sz % 3; // round up to 3
-        assert((sz << 1) <= out.size());
+        out.resize((sz << 1) * MachineConfig::bits);
 
-        auto alloc = std::pmr::polymorphic_allocator<Integer::value_type>{resource};
-        auto a = std::pmr::vector<uint_t>{sz, 0, resource};
-        auto b = std::pmr::vector<uint_t>{sz, 0, resource};
-
-        std::copy_n(lhs.data(), lhs.size(), a.begin());
-        std::copy_n(rhs.data(), rhs.size(), b.begin());
+        auto tmp = std::pmr::vector<Integer::value_type>{sz, 0, resource};
+        std::copy_n(a.data(), a.size(), tmp.begin());
 
         detail::toom_cook_3_helper<NaiveThreshold + (3 - NaiveThreshold % 3)>(
             out.data(), out.size(),
-            a.data(), b.data(),
+            tmp.data(), tmp.data(),
             sz
         );
     }
---
+++ include/big_num/internal/number_span.hpp
@@ -0,0 +1,199 @@
+#ifndef AMT_BIG_NUM_INTERNAL_NUMBER_SPAN_HPP
+#define AMT_BIG_NUM_INTERNAL_NUMBER_SPAN_HPP
+
+#include "base.hpp"
+#include <algorithm>
+#include <limits>
+#include <span>
+#include <type_traits>
+#include <utility>
+
+namespace big_num::internal {
+
+    namespace detail {
+        template <typename T>
+        struct is_std_span: std::false_type {};
+
+        template <typename T>
+        struct is_std_span<std::span<T>>: std::true_type {};
+
+        template <typename T> 
+        concept is_convertible_to_span = requires(T const& v) {
+            { std::span(v) };
+        } && !is_std_span<T>::value;
+
+        inline static constexpr auto calculate_bits_required(
+            std::span<MachineConfig::uint_t const> v
+        ) noexcept -> std::size_t {
+            if (v.empty()) return 0;
+            auto sz = v.size();
+            auto ptr = v.data();
+            auto i = sz;
+            for (; i > 0; --i) {
+                if (ptr[i - 1]) break;
+            }
+            if (i == 0) return 0;
+
+            auto blocks = i - 1;
+            auto bits = blocks * MachineConfig::bits;
+            return bits + static_cast<std::size_t>(std::bit_width(ptr[blocks]));
+        }
+    } // namespace detail
+
+    template <typename T>
+    struct NumberSpan {
+        using base_type = std::span<T>;
+        using value_type = typename base_type::value_type;
+        using size_type = typename base_type::size_type;
+        using difference_type = typename base_type::difference_type;
+        using reference = typename base_type::reference;
+        using const_reference = typename base_type::const_reference;
+        using pointer = typename base_type::pointer;
+        using const_pointer = typename base_type::const_pointer;
+        using iterator = typename base_type::iterator;
+        using reverse_iterator = typename base_type::reverse_iterator;
+
+        static constexpr auto npos = std::numeric_limits<size_type>::max();
+
+        constexpr NumberSpan() noexcept = default;
+        constexpr NumberSpan(NumberSpan const&) noexcept = default;
+        constexpr NumberSpan(NumberSpan &&) noexcept = default;
+        constexpr NumberSpan& operator=(NumberSpan const&) noexcept = default;
+        constexpr NumberSpan& operator=(NumberSpan &&) noexcept = default;
+        constexpr ~NumberSpan() noexcept = default;
+
+        constexpr NumberSpan(T* data, size_type size, bool is_neg = false, size_type bits = npos) noexcept
+            : m_base(data, size)
+            , m_is_neg(is_neg)
+            , m_bits(bits)
+        {}
+
+        constexpr NumberSpan(base_type base, bool is_neg = false, size_type bits = npos) noexcept
+            : m_base(base)
+            , m_is_neg(is_neg)
+            , m_bits(bits)
+        {}
+
+        // constexpr NumberSpan(NumberSpan<std::add_const_t<T>> const& other) noexcept requires (std::is_const_v<T>)
+        //     : m_base(other.m_base)
+        //     , m_is_neg(other.is_neg())
+        //     , m_bits(other.bits())
+        // {}
+
+        constexpr auto span() const noexcept -> std::span<std::add_const_t<value_type>> { return m_base; }
+        constexpr auto span() noexcept -> base_type requires (!std::is_const_v<T>) { return m_base; }
+        constexpr auto size() const noexcept -> size_type { return m_base.size(); }
+        constexpr auto empty() const noexcept -> bool { return m_base.empty(); }
+        constexpr auto set_neg(bool n) noexcept -> void { m_is_neg = n; }
+        constexpr auto is_neg() const noexcept -> bool { return m_is_neg; }
+        template <typename U>
+        constexpr auto copy_sign(NumberSpan<U> const& n) noexcept -> void { m_is_neg = n.is_neg(); }
+        constexpr operator std::span<std::add_const_t<value_type>>() const noexcept {
+            return m_base;
+        }
+        constexpr operator std::span<value_type>() noexcept requires (!std::is_const_v<T>) {
+            return m_base;
+        }
+
+        constexpr auto bits() const noexcept -> size_type {
+            if (m_bits == npos) return m_bits = detail::calculate_bits_required(m_base);
+            return m_bits;
+        }
+
+        constexpr auto begin() noexcept -> iterator { return m_base.begin(); }
+        constexpr auto end() noexcept -> iterator { return m_base.end(); }
+        constexpr auto rbegin() noexcept -> reverse_iterator { return m_base.rbegin(); }
+        constexpr auto rend() noexcept -> reverse_iterator { return m_base.rend(); }
+
+        constexpr auto begin() const noexcept -> iterator { return m_base.begin(); }
+        constexpr auto end() const noexcept -> iterator { return m_base.end(); }
+        constexpr auto rbegin() const noexcept -> reverse_iterator { return m_base.rbegin(); }
+        constexpr auto rend() const noexcept -> reverse_iterator { return m_base.rend(); }
+
+        constexpr auto data() noexcept -> pointer { return m_base.data(); }
+        constexpr auto data() const noexcept -> const_pointer { return m_base.data(); }
+
+        constexpr auto operator[](size_type k) noexcept -> reference { return m_base[k]; }
+        constexpr auto operator[](size_type k) const noexcept -> const_reference { return m_base[k]; }
+
+        constexpr auto trim_trailing_zeros() noexcept -> void {
+            auto i = size();
+            for (; i > 0; --i) {
+                if (m_base[i - 1]) break;
+            }
+            m_base = { data(), i };
+        }
+
+        constexpr auto slice(
+            size_type start,
+            size_type size = npos
+        ) noexcept -> NumberSpan requires (!std::is_const_v<T>) {
+            auto sz = std::min(size, this->size());
+            sz = std::max(start, sz) - start;
+            return { m_base.data() + start, sz, is_neg(), bits() };
+        }
+
+        constexpr auto slice(
+            size_type start,
+            size_type size = npos
+        ) const noexcept -> NumberSpan<std::add_const_t<value_type>> {
+            auto sz = std::min(size, this->size());
+            sz = std::max(start, sz) - start;
+            return { m_base.data() + start, sz, is_neg(), bits() };
+        }
+
+        constexpr auto abs() const noexcept -> NumberSpan {
+            return { m_base, false, bits() };
+        }
+
+        constexpr operator NumberSpan<std::add_const_t<value_type>>() const noexcept {
+            return { m_base, m_is_neg, m_bits };
+        }
+    private:
+        base_type m_base{};
+        bool m_is_neg{false};
+        mutable size_type m_bits{npos};
+    };
+
+    template <typename T>
+    NumberSpan(std::span<T>, bool, std::size_t) -> NumberSpan<T>;
+
+    template <typename T>
+    NumberSpan(T*, std::size_t, bool, std::size_t) -> NumberSpan<T>;
+
+    using const_num_t = NumberSpan<MachineConfig::uint_t const>;
+    using num_t = NumberSpan<MachineConfig::uint_t>;
+} // namespace big_num::internal
+
+#include <format>
+
+namespace std {
+    template <typename T>
+    struct std::formatter<big_num::internal::NumberSpan<T>> {
+        constexpr auto parse(auto& ctx) {
+            auto it = ctx.begin();
+            while (it != ctx.end()) {
+                if (*it == '}') break;
+                ++it;
+            }
+            return it;
+        }
+
+        auto format(big_num::internal::NumberSpan<T> const& n, auto& ctx) const {
+            auto out = ctx.out();
+            if (n.is_neg()) {
+                std::format_to(out, "-");
+            }
+            std::format_to(out, "[");
+            for (auto i = 0zu; i < n.size(); ++i) {
+                // std::format_to(out, "0x{:016x}", n[i]);
+                std::format_to(out, "{}", n[i]);
+                if (i + 1 < n.size()) std::format_to(out, ", ");
+            }
+            std::format_to(out, "]");
+            return out;
+        }
+    };
+} // namespace std
+
+#endif // AMT_BIG_NUM_INTERNAL_NUMBER_SPAN_HPP
